78c85105fc1f32a37791343b6207a7866e2021a6
Christian Johansen <christian.johansen@cpr.ku.dk>
2024-09-05 14:45:26 +0200

    Tab to open files and refocus

--- .config/nvim/lua/plugins/ui/neotree.lua
+++ .config/nvim/lua/plugins/ui/neotree.lua
@@ -1,143 +1,121 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  cmd = "Neotree",
-  keys = {
-    {
-      "<leader>fe",
-      function()
-        require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
-      end,
-      desc = "Explorer NeoTree (cwd)",
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    cmd = "Neotree",
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ source = 'filesystem', toggle = true, dir = vim.uv.cwd(), })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
+      { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+      {
+        "<leader>ge",
+        function()
+          require("neo-tree.command").execute({ source = "git_status", toggle = true })
+        end,
+        desc = "Git Explorer",
+      },
+      {
+        "<leader>be",
+        function()
+          require("neo-tree.command").execute({ source = "buffers", toggle = true })
+        end,
+        desc = "Buffer Explorer",
+      },
     },
-    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
-    {
-      "<leader>ge",
-      function()
-        require("neo-tree.command").execute({ source = "git_status", toggle = true })
-      end,
-      desc = "Git Explorer",
-    },
-    {
-      "<leader>be",
-      function()
-        require("neo-tree.command").execute({ source = "buffers", toggle = true })
-      end,
-      desc = "Buffer Explorer",
-    },
-  },
-  deactivate = function()
-    vim.cmd([[Neotree close]])
-  end,
-  init = function()
-    if vim.fn.argc(-1) == 1 then
-      local stat = vim.uv.fs_stat(vim.fn.argv(0))
-      if stat and stat.type == "directory" then
-        require("neo-tree")
+    deactivate = function()
+      vim.cmd([[Neotree close]])
+    end,
+    init = function()
+      if vim.fn.argc(-1) == 1 then
+        local stat = vim.uv.fs_stat(vim.fn.argv(0))
+        if stat and stat.type == "directory" then
+          require("neo-tree")
+        end
       end
-    end
-  end,
-  opts = {
-    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
-    open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
-    window = {
-      width = 60,
-      mappings = {
-        ["<space>"] = "none",
-        ["Y"] = {
-          function(state)
-            local node = state.tree:get_node()
-            local path = node:get_id()
-            vim.fn.setreg("+", path, "c")
-          end,
-          desc = "Copy Path to Clipboard",
-        },
-        ["o"] = "system_open"
-      },
-    },
-
-    filesystem = {
+    end,
+    opts = {
+      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+      open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
       window = {
-        position = "float",
+        width = 60,
+        mappings = {
+          ["<space>"] = "none",
+          ["<cr>"] = "open",
+          ["<tab>"] = {
+            function(state)
+              require("neo-tree.sources.common.commands").open(state)
+              require("neo-tree.command").execute({ action = "focus" })
+            end
+
+          },
+          ["Y"] = {
+            function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              vim.fn.setreg("+", path, "c")
+            end,
+            desc = "Copy Path to Clipboard",
+          },
+          ["o"] = "system_open"
+        },
       },
-      bind_to_cwd = false,
-      follow_current_file = { enabled = true },
-      use_libuv_file_watcher = true,
-      components = {
-        harpoon_index = function(config, node, _)
-          local harpoon_list = require("harpoon"):list()
+
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+      },
+      git_status = {
+        window = {
+          mappings = {
+            ["A"] = "git_add_all",
+            ["gy"] = "git_unstage_file",
+          },
+        },
+      },
+      commands = {
+        system_open = function(state)
+          local node = state.tree:get_node()
           local path = node:get_id()
-          local harpoon_key = vim.uv.cwd()
-
-          for i, item in ipairs(harpoon_list.items) do
-            local value = item.value
-            if string.sub(item.value, 1, 1) ~= "/" then
-              value = harpoon_key .. "/" .. item.value
-            end
-
-            if value == path then
-              vim.print(path)
-              return {
-                text = string.format(" ⥤ %d", i), -- <-- Add your favorite harpoon like arrow here
-                highlight = config.highlight or "NeoTreeDirectoryIcon",
-              }
-            end
-          end
-          return {}
+          vim.ui.open(path)
         end,
       },
-      renderers = {
-        file = {
-          { "icon" },
-          { "name",         use_git_status_colors = true },
-          { "harpoon_index" }, --> This is what actually adds the component in where you want it
-          { "diagnostics" },
-          { "git_status",   highlight = "NeoTreeDimText" },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = "",
+          expander_expanded = "",
+          expander_highlight = "NeoTreeExpander",
         },
       },
-    },
-    buffers = {
-      window = {
-        position = 'float'
-      }
-    },
-    git_status = {
-      window = {
-        mappings = {
-          ["A"] = "git_add_all",
-          ["gy"] = "git_unstage_file",
-        },
-      },
-    },
-    commands = {
-      system_open = function(state)
-        local node = state.tree:get_node()
-        local path = node:get_id()
-        vim.ui.open(path)
-      end,
-    },
-    default_component_configs = {
-      indent = {
-        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-        expander_collapsed = "",
-        expander_expanded = "",
-        expander_highlight = "NeoTreeExpander",
-      },
-    },
-    event_handlers = {
+      event_handlers = {
 
-      -- TODO: Should not apply for git view.
-      {
-        event = "file_open_requested",
-        handler = function(args)
-          -- auto close
-          -- vimc.cmd("Neotree close")
-          -- OR
-          if args.position == 'left' then
+        {
+          event = "file_open_requested",
+          handler = function()
+            -- auto close
+            -- vim.cmd("Neotree close")
+            -- OR
             require("neo-tree.command").execute({ action = "close" })
           end
-        end
-      },
+        },
+
+      }
     }
+  },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-neo-tree/neo-tree.nvim",
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
   },
 }
