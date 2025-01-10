84037b69f411ec9747bd70715028e93814d46a15
Christian Johansen <christian.johansen@cpr.ku.dk>
2025-01-10 22:51:56 +0100

    cleanup

--- wezterm/dot-config/wezterm/config/keys.lua
+++ wezterm/dot-config/wezterm/config/keys.lua
@@ -192,18 +192,6 @@ end
 
 ---@param config Config
 function M.setup(config)
-  wezterm.on('ActivatePaneDirection-right', function(window, pane)
-    conditionalActivatePane(window, pane, 'Right', 'l')
-  end)
-  wezterm.on('ActivatePaneDirection-left', function(window, pane)
-    conditionalActivatePane(window, pane, 'Left', 'h')
-  end)
-  wezterm.on('ActivatePaneDirection-up', function(window, pane)
-    conditionalActivatePane(window, pane, 'Up', 'k')
-  end)
-  wezterm.on('ActivatePaneDirection-down', function(window, pane)
-    conditionalActivatePane(window, pane, 'Down', 'j')
-  end)
   key_configurations(config)
 end
 
