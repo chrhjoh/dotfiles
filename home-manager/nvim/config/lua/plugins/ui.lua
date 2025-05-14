e8a0b873c79c97b0f95c5daee2c5325c051997e1
Christian Johansen <christian.holm.johansen@gmail.com>
2025-05-14 09:05:30 +0100

    updates to neovim messages

--- home-manager/nvim/config/lua/plugins/ui.lua
+++ home-manager/nvim/config/lua/plugins/ui.lua
@@ -78,8 +78,8 @@ return {
       return noice_map {
         { "<c-Enter>",  function()   require("noice").redirect(vim.fn.getcmdline()) end,                        desc = "Redirect Cmdline",  mode = "c"},
         { "<leader>nl", function()   require("noice").cmd("last") end,                                          desc = "Noice Last Message",},
-        { "<leader>nh", function()   require("noice").cmd("history") end,                                       desc = "Noice History",},
         { "<leader>na", function()   require("noice").cmd("all") end,                                           desc = "Noice All",},
+        { "<leader>N",  function()   require("noice").cmd("all") end,                                           desc = "Noice All",},
         { "<leader>nd", function()   require("noice").cmd("dismiss") end,                                       desc = "Dismiss All",},
         { "<leader>sn", function()   require("noice").cmd("pick") end,                                          desc = "Search",},
         { "<c-f>",      function()   if not require("noice.lsp").scroll(4) then     return "<c-f>"   end end,   desc = "Scroll Forward",    mode = { "i", "n", "s" },  silent = true, expr = true, },
--- home-manager/nvim/config/lua/plugins/utils.lua
+++ home-manager/nvim/config/lua/plugins/utils.lua
@@ -113,7 +113,6 @@ return {
           _G.bt = function()
             Snacks.debug.backtrace()
           end
-          vim.print = _G.dd -- Override print to use snacks for `:=` command
         end,
       })
       vim.api.nvim_create_autocmd("FileType", {
