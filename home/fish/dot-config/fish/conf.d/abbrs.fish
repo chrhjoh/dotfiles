c8b5910fadba36f36082a3a49262b25b36012f46
Christian Johansen <christian.holm.johansen@gmail.com>
2025-07-26 11:15:30 +0200

    add fish abbr

--- home/fish/dot-config/fish/conf.d/abbrs.fish
+++ home/fish/dot-config/fish/conf.d/abbrs.fish
@@ -1,3 +1,4 @@
 abbr --add --global pythondir 'echo "layout python3" > .envrc; direnv allow'
 abbr --add --global homestow 'stow --dir $DOTS/home --target $HOME --no-folding --dotfiles'
 abbr --add --global lazydots 'lazygit -p $DOTS'
+abbr --add --global dots '$DOTS'
