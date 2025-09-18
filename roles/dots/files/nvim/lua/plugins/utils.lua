0a0316ed38513cc8cb35a0e6e4005db0b2b73230
Christian Johansen <christian.holm.johansen@gmail.com>
2025-09-18 17:37:23 +0200

    change from mini surround to nvim surround

--- roles/dots/files/nvim/lua/plugins/utils.lua
+++ roles/dots/files/nvim/lua/plugins/utils.lua
@@ -223,19 +223,8 @@ return {
     opts = { enabled = false },
   },
   {
-    "nvim-mini/mini.surround",
+    "kylechui/nvim-surround",
     event = "VeryLazy",
-    opts = {
-      search_method = "nearest",
-      mappings = {
-        add = "gsa", -- Add surrounding in Normal and Visual modes
-        delete = "gsd", -- Delete surrounding
-        find = "gsf", -- Find surrounding (to the right)
-        find_left = "gsF", -- Find surrounding (to the left)
-        highlight = "gsh", -- Highlight surrounding
-        replace = "gsr", -- Replace surrounding
-        update_n_lines = "gsn", -- Update `n_lines`
-      },
-    },
+    opts = {},
   },
 }
