be4bb3b098680d2ededb03d21f6e4cb143063335
Christian Johansen <christian.holm.johansen@gmail.com>
2026-09-19 15:05:39 +0200

    add background images

--- fish/dot-config/fish/conf.d/rustup.fish
+++ fish/dot-config/fish/conf.d/rustup.fish
@@ -1,3 +1,3 @@
-if test -f ~/.local/share/cargo/env.fish
-    source ~/.local/share/cargo/env.fish
+if type -q brew; and test -d (brew --prefix)/opt/rustup/bin
+    fish_add_path (brew --prefix)/opt/rustup/bin
 end
